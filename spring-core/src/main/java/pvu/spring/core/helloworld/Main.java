cb8e8f63cfc2bd2649cbe7664a4777b1c2b7a8b0
Phat Vu <phat.vu@axonactive.com>
2017-06-09 18:39:11 +0700

    Add more new hello-world example

--- spring-core/src/main/java/pvu/spring/core/helloworld/Main.java
+++ spring-core/src/main/java/pvu/spring/core/helloworld/Main.java
@@ -10,7 +10,22 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("pvu/spring/core/helloworld/bean.xml");
+
         HelloWorld bean = context.getBean("helloWorld", HelloWorld.class);
+        // hello world
         bean.sayHi();
+
+        // a bit more 'hello world'
+        Account account1 = context.getBean("account1", Account.class);
+        System.out.println("\n=>Account 1 data:");
+        System.out.println("ID: " + account1.getId());
+        System.out.println("Account name: " + account1.getOwnerName());
+        System.out.println("Balance: " + account1.getBalance());
+
+        Account account2 = context.getBean("account2", Account.class);
+        System.out.println("\n=>Account 2 data:");
+        System.out.println("ID: " + account2.getId());
+        System.out.println("Account name: " + account2.getOwnerName());
+        System.out.println("Balance: " + account2.getBalance());
     }
 }
